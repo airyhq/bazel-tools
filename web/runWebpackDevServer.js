2577f95b79aeef4c70a6aad1805b19ec707dbfa2
Christoph Proeschel <proeschel.christoph@gmail.com>
2021-04-08 16:04:11 +0200

    Fix hot code reloading (#38)

--- web/runWebpackDevServer.js
+++ web/runWebpackDevServer.js
@@ -1,58 +1,37 @@
+require(require.resolve('html-webpack-plugin', {paths: [process.cwd()]}));
+
 const path = require('path');
-
 const argv = require('minimist')(process.argv.slice(2));
-
 const configGenerator = require(path.resolve(argv.config));
-console.log('cwd', process.cwd());
 
 const config = configGenerator(process.env, argv);
-
+const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 
-const port = 8080;
+const publicPath = JSON.parse(argv.outputDict || "{}").publicPath || '/';
 
-const middleware = require('webpack-dev-middleware');
-const compiler = webpack(config);
-const express = require('express');
-const app = express();
-
-const instance = middleware(compiler, {
-  publicPath: config.output.publicPath,
-  stats: {colors: true},
-});
-
-app.use(
-  require('connect-history-api-fallback')({
-    index: config.output.publicPath + "index.html",
-    rewrites: {
-      from: new RegExp(config.output.publicPath + '[^.]*$'),
-      to: config.output.publicPath + "index.html",
+const options = {
+    // TODO If we ever want to serve static assets for the devserver
+    contentBase: false,
+    hot: true,
+    host: 'localhost',
+    historyApiFallback: {
+        rewrites: [
+            {
+                from: new RegExp(config.output.publicPath + '[^.]*$'),
+                to: config.output.publicPath + "index.html",
+            }
+        ],
     },
-  })
-);
+    publicPath
+};
 
-app.use(instance);
+webpackDevServer.addDevServerEntrypoints(config, options);
+const compiler = webpack(config);
+const server = new webpackDevServer(compiler, options);
 
-app.use(require('webpack-hot-middleware')(compiler));
+const port = process.env.PORT || 8080;
 
-// After the first build, we immediately stop the built-in middleware watcher
-// as we will manually retrigger builds using ibazel
-instance.waitUntilValid(() => {
-  instance.close();
+server.listen(port, 'localhost', () => {
+    console.log(`dev server listening on port ${port}`);
 });
-
-// Listen for ibazel commands on stdin
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', () => {
-  let chunk;
-  // Use a loop to make sure we read all available data.
-  while ((chunk = process.stdin.read()) !== null) {
-    if (chunk.includes('IBAZEL_BUILD_COMPLETED SUCCESS')) {
-      console.log('IBAZEL triggered an invalidation');
-      instance.invalidate();
-    }
-  }
-});
-
-app.listen(port, () => console.log('Webpack app listening on port ' + port));
--- web/webpack.dev.config.js
+++ web/webpack.dev.config.js
@@ -1,118 +1,131 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const cwdRequire = id => require(require.resolve(id, { paths: [process.cwd()] }));
 
-// For dev the tsconfig-paths-webpack-plugin doesn't cut it, as it links to file locations
-// that are only available to build rules
+const webpack = cwdRequire('webpack');
+const HtmlWebpackPlugin = cwdRequire("html-webpack-plugin")
+
 function resolveTsconfigPathsToAlias({tsconfigPath, basePath}) {
-  const {paths} = require(tsconfigPath).compilerOptions;
+    const {paths} = require(tsconfigPath).compilerOptions;
 
-  const stripGlobs = path => path.replace('/*', '');
+    const stripGlobs = path => path.replace('/*', '');
 
-  return Object.keys(paths).reduce((aliases, moduleMappingKey) => {
-    const key = stripGlobs(moduleMappingKey);
-    const value = path.resolve(basePath, stripGlobs(paths[moduleMappingKey][0]).replace('*', ''));
+    return Object.keys(paths).reduce((aliases, moduleMappingKey) => {
+        const key = stripGlobs(moduleMappingKey);
+        const value = path.resolve(basePath, stripGlobs(paths[moduleMappingKey][0]).replace('*', ''));
 
-    return {
-      ...aliases,
-      [key]: value,
-    };
-  }, {});
+        return {
+            ...aliases,
+            [key]: value,
+        };
+    }, {});
 }
 
 module.exports = (env, argv) => {
-  const output = {
-    publicPath: '/',
-    ...JSON.parse(argv.outputDict || "{}"),
-  };
+    const output = {
+        publicPath: '/',
+        filename: 'js/[name].[chunkhash:8].js',
+        ...JSON.parse(argv.outputDict || "{}"),
+    };
 
-  return ({
-    mode: 'development',
-    target: 'web',
-    bail: false,
+    return {
+        context: env.BUILD_WORKSPACE_DIRECTORY,
 
-    entry: ['react-hot-loader/patch', 'webpack-hot-middleware/client', path.resolve(argv.entry)],
+        mode: 'development',
+        target: 'web',
+        bail: false,
 
-    output,
+        entry: ['react-hot-loader/patch', path.resolve(argv.entry)],
 
-    optimization: {
-      minimize: false,
-    },
+        output,
 
-    resolve: {
-      alias: resolveTsconfigPathsToAlias({
-        tsconfigPath: path.resolve(argv.tsconfig),
-        basePath: process.cwd(),
-      }),
-    },
-
-    devtool: 'eval',
-
-    module: {
-      rules: [
-        {
-          test: /\.(mjs|js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: 'entry',
-                  corejs: 3,
-                  modules: false,
-                  targets: ['>0.2%', 'not dead', 'not op_mini all'],
-                },
-              ],
-            ],
-          },
+        optimization: {
+            minimize: false,
         },
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            'style-loader',
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                modules: {
-                  auto: true,
-                  localIdentName: '[name]_[local]-[hash:base64:5]',
-                },
-              },
+
+        resolve: {
+            alias: {
+                ...resolveTsconfigPathsToAlias({
+                    tsconfigPath: path.resolve(argv.tsconfig),
+                    basePath: process.cwd(),
+                }),
+                ...JSON.parse(argv.aliases || "{}")
             },
-            'sass-loader',
-          ],
+            extensions: ['.tsx', '.ts', '.js'],
         },
-        {
-          test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-          loader: 'file-loader',
-          options: {
-            name: 'media/[name].[hash:8].[ext]',
-          },
-        },
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: '@svgr/webpack',
-              options: {
-                titleProp: true,
-                svgoConfig: {
-                  plugins: {
-                    removeViewBox: false,
-                  },
+
+        devtool: 'eval-cheap-module-source-map',
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: 'entry',
+                                    modules: false,
+                                    corejs: 3,
+                                    targets: ['>0.2%', 'not dead', 'not op_mini all'],
+                                },
+                            ],
+                            '@babel/preset-react',
+                            ['@babel/preset-typescript', {isTSX: true, allExtensions: true}],
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-spread',
+                            '@babel/plugin-proposal-object-rest-spread',
+                            '@babel/plugin-proposal-class-properties',
+                        ],
+                    },
                 },
-                template: ({template}, opts, {imports, interfaces, componentName, props, jsx, exports}) => {
-                  const plugins = ['jsx'];
-                  if (opts.typescript) {
-                    plugins.push('typescript');
-                  }
-                  const typeScriptTpl = template.smart({plugins});
-                  return typeScriptTpl.ast`
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: {
+                                    auto: true,
+                                    localIdentName: '[name]_[local]-[hash:base64:5]',
+                                },
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: 'media/[name].[hash:8].[ext]',
+                    },
+                },
+                {
+                    test: /\.svg$/,
+                    use: [
+                        {
+                            loader: '@svgr/webpack',
+                            options: {
+                                titleProp: true,
+                                svgoConfig: {
+                                    plugins: {
+                                        removeViewBox: false,
+                                    },
+                                },
+                                template: ({template}, opts, {imports, interfaces, componentName, props, jsx, exports}) => {
+                                    const plugins = ['jsx'];
+                                    if (opts.typescript) {
+                                        plugins.push('typescript');
+                                    }
+                                    const typeScriptTpl = template.smart({plugins});
+                                    return typeScriptTpl.ast`
                                       ${imports}
                                       ${interfaces}
                                       function ${componentName}(${props}) {
@@ -121,43 +134,27 @@ module.exports = (env, argv) => {
                                       }
                                       ${exports}
                                       `;
+                                },
+                            },
+                        },
+                        // Use url-loader to be able to inject into img src
+                        // https://www.npmjs.com/package/@svgr/webpack#using-with-url-loader-or-file-loader
+                        'url-loader',
+                    ],
                 },
-              },
-            },
-            // Use url-loader to be able to inject into img src
-            // https://www.npmjs.com/package/@svgr/webpack#using-with-url-loader-or-file-loader
-            'url-loader',
-          ],
+            ],
         },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': `"development"`,
-        'process.env.PUBLIC_PATH': `'${output.publicPath}'`,
-        ...JSON.parse(argv.defines || "{}"),
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: '**/public/**/*',
-            noErrorOnMissing: true,
-            globOptions: {
-              ignore: ['**/node_modules/**'],
-              transformPath(targetPath) {
-                const splits = targetPath.split('public/');
-                return splits[1];
-              },
-            }
-          },
-        ]
-      }),
-      new HtmlWebpackPlugin({
-        template: path.resolve(argv.index),
-        inject: true,
-        filename: 'index.html',
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-    ],
-  });
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': `"development"`,
+                'process.env.PUBLIC_PATH': `'${output.publicPath}'`,
+                ...JSON.parse(argv.defines || '{}'),
+            }),
+            new HtmlWebpackPlugin({
+                template: argv.index,
+                inject: true,
+                filename: 'index.html',
+            }),
+        ],
+    };
 };
